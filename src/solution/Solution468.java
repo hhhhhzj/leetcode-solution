cc2d9ab03d2fde27e62abcd1b21f550fe4b13a5d
Zhijian.H <huangzhijian02@meituan.com>
2021-05-03 19:01:56 +0800

    添加468解法

---
+++ src/solution/Solution468.java
@@ -0,0 +1,160 @@
+package solution;
+
+//Given a string IP, return "IPv4" if IP is a valid IPv4 address, "IPv6" if IP i
+//s a valid IPv6 address or "Neither" if IP is not a correct IP of any type.
+//
+// A valid IPv4 address is an IP in the form "x1.x2.x3.x4" where 0 <= xi <= 255
+//and xi cannot contain leading zeros. For example, "192.168.1.1" and "192.168.1.0
+//" are valid IPv4 addresses but "192.168.01.1", while "192.168.1.00" and "192.168
+//@1.1" are invalid IPv4 addresses.
+//
+// A valid IPv6 address is an IP in the form "x1:x2:x3:x4:x5:x6:x7:x8" where:
+//
+//
+// 1 <= xi.length <= 4
+// xi is a hexadecimal string which may contain digits, lower-case English lette
+//r ('a' to 'f') and upper-case English letters ('A' to 'F').
+// Leading zeros are allowed in xi.
+//
+//
+// For example, "2001:0db8:85a3:0000:0000:8a2e:0370:7334" and "2001:db8:85a3:0:0
+//:8A2E:0370:7334" are valid IPv6 addresses, while "2001:0db8:85a3::8A2E:037j:7334
+//" and "02001:0db8:85a3:0000:0000:8a2e:0370:7334" are invalid IPv6 addresses.
+//
+//
+// Example 1:
+//
+//
+//Input: IP = "172.16.254.1"
+//Output: "IPv4"
+//Explanation: This is a valid IPv4 address, return "IPv4".
+//
+//
+// Example 2:
+//
+//
+//Input: IP = "2001:0db8:85a3:0:0:8A2E:0370:7334"
+//Output: "IPv6"
+//Explanation: This is a valid IPv6 address, return "IPv6".
+//
+//
+// Example 3:
+//
+//
+//Input: IP = "256.256.256.256"
+//Output: "Neither"
+//Explanation: This is neither a IPv4 address nor a IPv6 address.
+//
+//
+// Example 4:
+//
+//
+//Input: IP = "2001:0db8:85a3:0:0:8A2E:0370:7334:"
+//Output: "Neither"
+//
+//
+// Example 5:
+//
+//
+//Input: IP = "1e1.4.5.6"
+//Output: "Neither"
+//
+//
+//
+// Constraints:
+//
+//
+// IP consists only of English letters, digits and the characters '.' and ':'.
+//
+// Related Topics String
+// 👍 477 👎 1999
+
+
+import com.sun.deploy.util.StringUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Zhijian.H
+ * @since 2021/5/3 下午6:05
+ */
+public class Solution468 {
+
+    public String validIPAddress(String IP) {
+        if (isValidIpV4(IP)) {
+            return "IpV4";
+        } if (isValidIpV6(IP)) {
+            return "IpV6";
+        } else {
+            return "Neither";
+        }
+    }
+
+    /**
+     * 是否是有效Ipv4地址
+     *
+     * @param IP
+     * @return
+     */
+    private boolean isValidIpV4(String IP) {
+        if (IP.chars().filter(ch -> ch == '.').count() != 3) {
+            return false;
+        }
+        List<String> strList = Arrays.asList(IP.split("\\."));
+        if (strList == null || strList.size() != 4) {
+            return false;
+        }
+        for (String str : strList) {
+            if (str.charAt(0) == '0' || str.length() > 3) {
+                return false;
+            }
+            for (char ch : str.toCharArray()) {
+                if (!Character.isDigit(ch)) {
+                    return false;
+                }
+            }
+            if (Integer.parseInt(str) > 255) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 是否是有效Ipv6地址
+     *
+     * @param IP
+     * @return
+     */
+    private boolean isValidIpV6(String IP) {
+        if (IP.chars().filter(ch -> ch == ':').count() != 7) {
+            return false;
+        }
+        List<String> strList = Arrays.asList(IP.split("\\:"));
+        if (strList == null || strList.size() != 8) {
+            return false;
+        }
+        final String hexdigits = "0123456789abcdefABCDEF";
+        for (String str : strList) {
+            if (str.length() > 4) {
+                return false;
+            }
+            for (char ch : str.toCharArray()) {
+                if (hexdigits.indexOf(ch) == -1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution468 solition = new Solution468();
+        System.out.println(solition.validIPAddress("172.16.254.1"));
+        System.out.println(solition.validIPAddress("2001:0db8:85a3:0:0:8A2E:0370:7334"));
+        System.out.println(solition.validIPAddress("256.256.256.256"));
+        System.out.println(solition.validIPAddress("2001:0db8:85a3:0:0:8A2E:0370:7334:"));
+        System.out.println(solition.validIPAddress("1e1.4.5.6"));
+    }
+}
